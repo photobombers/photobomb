12c1490b6ed1e3b993201fc27cf4ffa48cf39f01
Paul Amazona <whatevergeek@gmail.com>
2020-11-17 18:36:40 +0800

    Added initial files for PhotoBombDetector

---
+++ PhotoBombDetector/Detector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using Microsoft.ML;
+
+using PhotoBombDetector.YoloParser;
+using PhotoBombDetector.DataStructures;
+
+namespace PhotoBombDetector
+{
+    public class Detector
+    {
+        private List<(string Label, float Score)> GetDetectedObjects(IList<YoloBoundingBox> boundingBoxes)
+        {
+            List<(string Label, float Score)> detectedList = new List<(string Label, float Score)>();
+
+            foreach (var box in boundingBoxes)
+            {
+                detectedList.Add((box.Label, box.Confidence));
+            }
+
+            return detectedList;
+        }
+    }
+}
